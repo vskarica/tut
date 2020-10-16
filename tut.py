cd2cfead30db1d9ca44499a83f9313413ff78d85
vskarica <vskarica@gmail.com>
2020-10-16 10:31:45 +0200

    promjena boje

--- tut.py
+++ tut.py
@@ -55,5 +55,5 @@ while run:
             jumpCount=jumpCountMax       
     
     win.fill((0,0,0))
-    pygame.draw.rect(win,(255,0,0),(x,y,width,height))
+    pygame.draw.rect(win,(0,255,0),(x,y,width,height))
     pygame.display.update()
